02f7de4fbb38f245ea55b7172e9a83693bc8f82a
ThatsMyName92 <69623155+ThatsMyName92@users.noreply.github.com>
2021-05-05 13:51:51 +0300

    E2

--- Exercise/E2.py
+++ Exercise/E2.py
@@ -3,8 +3,8 @@ Create 2 variables: string of your full name and string of your email.
 Create variabble of your age (int)
 print all of them to the screen
 then print your name from the end to the beginning and print it only with age*3.
-then check if your full name is stored inside this full string:
-"idan ben dudu yuval shimpn yael gal adam shahar yana"
+then check if your name is stored inside this full string:
+"idan ben dudu yuval shimon yael gal adam shahar yana"
 '''
 
 n="iliya yeriskin"
