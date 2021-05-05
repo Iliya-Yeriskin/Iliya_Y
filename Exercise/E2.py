839b7d9316852f1a47238987f6d611202f666ac2
ThatsMyName92 <69623155+ThatsMyName92@users.noreply.github.com>
2021-05-05 14:00:06 +0300

    E2

--- Exercise/E2.py
+++ Exercise/E2.py
@@ -6,16 +6,23 @@ then print your name from the end to the beginning and print it only with age*3.
 then check if your name is stored inside this full string:
 "idan ben dudu yuval shimon yael gal adam shahar yana"
 '''
-
-n="iliya yeriskin"
+from time import sleep
+n="Iliya Yeriskin"
 e="iliya@gmail.com"
 a=28
 #print my name+email+age
-print(n + "\n" + e +"\n" + str(a))
+print("Full name: " + n + "\n" + "Email: " + e +"\n" + "Age: " + str(a))
 #print my name backward and age*3
-print(n[::-1] + "\n" + str(a*3))
+print("Full name Backwards: " + n[::-1] + "\n" + "Age*3: " + str(a*3))
 #check if my name is in the string:
-list="idan ben dudu yuval shimpn yael gal adam shahar yana"
+list="idan ben dudu yuval shimon yael gal adam shahar yana"
+print("Checikng if your name is in the list...")
+print("...")
+sleep(0.5)
+print("...")
+sleep(0.5)
+print("...")
+sleep(0.5)
 print("iliya" in list)
 
 
