95cb838348cae97bb00a2082d9c8cbbabfd52b70
ThatsMyName92 <69623155+ThatsMyName92@users.noreply.github.com>
2021-05-11 18:00:35 +0300

    L6

--- Class work/L6-Conditions.py
+++ Class work/L6-Conditions.py
@@ -1,9 +1,9 @@
 #Conditions
 name=input("Enter a name: ")
-if(name=="iliya"):
+if(name=="iliya"):#checking if the name that was inputed is correct
     print("Hello Iliya\n")
     age = int(input("Enter a age: "))
-    if (age == 28):
+    if (age == 28):#check if age is correct
         print("Great, you may enter\n")
     else:
         print("Sorry, you are too young\n")
