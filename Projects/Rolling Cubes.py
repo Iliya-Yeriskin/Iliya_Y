617ddff142a2cd17865d9890ee67099a84e632c0
ThatsMyName92 <69623155+ThatsMyName92@users.noreply.github.com>
2021-05-18 18:48:08 +0300

    P3

--- Projects/Rolling Cubes.py
+++ Projects/Rolling Cubes.py
@@ -12,43 +12,48 @@ in the end we'll print how much money the player won.
 from random import randint
 from time import sleep
 print("Welcome to the Rolling Cube Game\n--------------------------------\nEach round costs 3₪\n")
-money=input("How much money do you have?: ")
-startmoney=money
-turns=int(money)//3
+money = input("How much money do you have?: \n")
+start_money = money
+turns = int(money)//3
+change = int(money) % 3
+print("Your Change is: "+str(change)+"₪")
 print("Prepare to play: "+str(turns)+" Rounds\n-----------------------")
-money=int(turns)*3
-cube1=0
-cube2=0
+money = int(turns)*3
+cube1 = 0
+cube2 = 0
 for i in range(turns):
-    startmoney=int(int(startmoney)-3)
-    money=int(int(money)-3)
+    start_money = int(int(start_money)-3)
+    money = int(int(money)-3)
     print("Round: "+str(i+1)+" Rolling...")
     sleep(2)
-    cube1=randint(1,6)
-    cube2=randint(1,6)
-    if cube1==cube2 & cube1==6:
+    cube1 = randint(1, 6)
+    cube2 = randint(1, 6)
+    if cube1 == cube2 & cube1 == 6:
         print("You Rolled ("+str(cube1)+","+str(cube2)+") And Won 1000₪!!\n-----------------------")
-        money=money+1000
-    elif cube1==cube2:
-        if cube1==1:
+        money = money+1000
+    elif cube1 == cube2:
+        if cube1 == 1:
             print("You Rolled ("+str(cube1)+","+str(cube2)+") And Won 120₪\n-----------------------")
-            money=money+120
-        elif cube2==2:
+            money = money+120
+        elif cube2 == 2:
             print("You Rolled ("+str(cube1)+","+str(cube2)+") And Won 140₪\n-----------------------")
-            money=money+140
+            money = money+140
         else:
             print("You Rolled ("+str(cube1)+","+str(cube2)+") And Won 100₪\n-----------------------")
-            money=money+100
-    elif cube1==1:
-        if cube2==2:
+            money = money+100
+    elif cube1 == 1:
+        if cube2 == 2:
             print("You Rolled ("+str(cube1)+","+str(cube2)+") And Won 60₪\n-----------------------")
-            money=money+60
+            money = money+60
         else:
             print("You Rolled ("+str(cube1)+","+str(cube2)+") And Won 20₪\n-----------------------")
-            money=money+20
-    elif cube2==2:
+            money = money+20
+    elif cube2 == 2:
         print("You Rolled (" + str(cube1) + "," + str(cube2) + ") And Won 40₪\n-----------------------")
-        money=money+40
+        money = money+40
     else:
         print("You Rolled ("+str(cube1)+","+str(cube2)+") Sorry you didn't win\n-----------------------")
-print("Your Total winning are: ["+str(money)+"₪]\nNow you have: ["+str(int(startmoney)+int(money))+"₪]"+"\nHope to see you again :-)")
+print("Calculating your Prize....\n")
+sleep(3)
+print("Your Total winning are: ["+str(money)+"₪]\nNow you have: ["+str(int(start_money)+int(money))+"₪]" +
+      "\nHope to see you again :-)")
