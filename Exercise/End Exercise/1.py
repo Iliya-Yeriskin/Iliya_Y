dbf930f6641c4b8cde7961043d4e28f96c824191
ThatsMyName92 <69623155+ThatsMyName92@users.noreply.github.com>
2021-05-25 18:01:01 +0300

    End Exe

--- Exercise/End Exercise/1.py
+++ Exercise/End Exercise/1.py
@@ -3,12 +3,16 @@ Write a Python program to print the following string in a specific format (see t
 Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in
 the sky. Twinkle, twinkle, little star, How I wonder what you are" Output :
 Twinkle, twinkle, little star,
-How I wonder what you are!
-Up above the world so high,
-Like a diamond in the sky.
+     How I wonder what you are!
+           Up above the world so high,
+           Like a diamond in the sky,
 Twinkle, twinkle, little star,
-How I wonder what you are
+     How I wonder what you are
 '''
 
-print("Twinkle, twinkle, little star,\n     How I wonder what you are!\n            Up above the world so high,\n"
-      "             Like a diamond in the sky.\nTwinkle, twinkle, little star,\n        How I wonder what you are.")
+print('''Twinkle, twinkle, little star,
+     How I wonder what you are!
+           Up above the world so high,
+           Like a diamond in the sky,
+Twinkle, twinkle, little star,
+     How I wonder what you are.''')
