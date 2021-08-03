6d63c44ddc8cf50ca4533a041116a9cd5165304f
ThatsMyName92 <iliya123@gmail.com>
2021-08-03 12:45:02 +0300

    Docker

--- DevOps/Intro/Docker/copy_from_servers.sh
+++ DevOps/Intro/Docker/copy_from_servers.sh
@@ -2,7 +2,7 @@
 
 echo "Select server ubuntu1/ubuntu2/centos:"
 read server
-echo "Select file to copy(full path after /home/iliya):"
+echo "Select file to copy(full path after /home/iliya/):"
 read path
 scp iliya@$server:/home/iliya/$path .
 sleep 1
