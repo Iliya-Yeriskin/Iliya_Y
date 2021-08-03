9791ddfdda4e0b45476f7db6318ef333963f26b1
ThatsMyName92 <iliya123@gmail.com>
2021-08-03 12:34:11 +0300

    Docker

---
+++ DevOps/Intro/Docker/copy_from_servers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Select server ubuntu1/ubuntu2/centos:"
+read server
+echo "Select file to copy(full path after /home/iliya):"
+read path
+scp iliya@$server:/home/iliya/$path .
+sleep 1
+echo "Done coping file to PC"
